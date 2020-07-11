813e688ca752c508a327a1748769c6e8d51228fa
stoneskin <wsun@ipipeline.com>
2020-07-11 18:34:02 -0400

    implement the message sections and delay with scheduler

--- src/main/java/mc/stoneskin/WelcomePlugin/MessageHelper.java
+++ src/main/java/mc/stoneskin/WelcomePlugin/MessageHelper.java
@@ -1,5 +1,7 @@
 package mc.stoneskin.WelcomePlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -8,10 +10,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class MessageHelper {
-	//private File file;
+	// private File file;
 	private FileConfiguration config;
 	private Plugin plugin;
-		
 
 	public MessageHelper(final Plugin plugin) {
 		this.plugin = plugin;
@@ -23,40 +24,91 @@ public class MessageHelper {
 		config = plugin.getConfig();
 	}
 
-	private String[] GetMessages(final Player player) {
-		// todo, support message same in array
-		String msg = config.getString("welcome.message");
-		if (msg != null) {
-			Bukkit.getServer().getLogger().info("Welcome msg loaded:" + msg);
-			System.out.print("msg = " + msg);
-			msg = msg.replaceAll("&", "§").replaceAll("%p", player.getName());
-			String lb = config.getString("welcome.linebreak");
-			if (lb == null)
-				lb = "\\n";
-			final String[] s = msg.split(lb);
-			return s;
+	// todo remove
+	// private String[] GetMessages(final Player player) {
+	// // todo, support message same in array
+	// String msg = config.getString("welcome.message");
+	// if (msg != null) {
+	// Bukkit.getServer().getLogger().info("Welcome msg loaded:" + msg);
+	// System.out.print("msg = " + msg);
+	// msg = msg.replaceAll("&", "§").replaceAll("%p", player.getName());
+	// String lb = config.getString("welcome.linebreak");
+	// if (lb == null)
+	// lb = "\\n";
+	// final String[] s = msg.split(lb);
+	// return s;
+	// }
+	// return new String[] {};
+	// }
+
+	private List<String> GetMessageSections() {
+		try {
+			String defaultMsg = config.getString("welcome.message");
+			List<String> msgSections = config.getStringList("welcome.messages");
+			if (defaultMsg != null)
+				msgSections.add(0, defaultMsg);
+			return msgSections;
+		} catch (Exception ex) {
+			String err = ex.getMessage();
+			ArrayList<String> re = new ArrayList<String>();
+			re.add("&4" + err + "&r");
+			return re;
 		}
-		return new String[] {};
+	}
+
+	private String[] FormateMessage(Player player, String msg) {
+		msg = msg.replaceAll("&", "§").replaceAll("%p", player.getName());
+		String lb = config.getString("welcome.linebreak");
+		if (lb == null)
+			lb = "\\n";
+		final String[] s = msg.split(lb);
+		return s;
 	}
 
 	public void SendWelcomeMessages(final Player player) {
-		final String[] msgs = GetMessages(player);
-		if (msgs.length > 0) {
+		List<String> messages = GetMessageSections();
+		int delay =config.getInt("welcome.delay");
+		if(delay<1)
+			delay=1;
+		for (int i = 0; i < messages.size(); i++) {
+			final String msg =messages.get(i);
+			Runnable task = new Runnable() {
+				@Override
+				public void run() {
 
-			for (final String m : msgs) {
-				player.sendMessage(m);
-			}
+					SendMessage(player, msg);
+				}
+			};
+			Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, task, 20*delay*i);
 
-		} else {
-			//send default message
-			player.sendMessage(("&5Welcome, &r&2%p&r&5 !").replaceAll("&", "§").replaceAll("%p", player.getName()));
 		}
 
 	}
+
+	private void SendMessage(final Player player, String msg) {
+		String[] outs = FormateMessage(player, msg);
+
+		for (String s : outs) {
+			player.sendMessage(s);
+		}
+	}
+	// final String[] msgs = GetMessages(player);
+	// if (msgs.length > 0) {
+
+	// for (final String m : msgs) {
+	// player.sendMessage(m);
+	// }
+
+	// } else {
+	// //send default message
+	// player.sendMessage(("&5Welcome, &r&2%p&r&5 !").replaceAll("&",
+	// "§").replaceAll("%p", player.getName()));
+	// }
+
+	// }
 
 	public FileConfiguration getConfig() {
 		return config;
 	}
-
 
 }
